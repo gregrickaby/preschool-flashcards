1fb3aaab1d8191777d012a231bc804f9cf532d90
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 15:31:41 -0500

    absolute

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 const Footer = () => (
-  <footer className="border-t bg-white">
+  <footer className="absolute bottom-0 right-0 left-0 border-t bg-white">
     <section className="flex flex-col justify-center items-center py-2">
       <Link href="/">
         <a>&larr; Back</a>
