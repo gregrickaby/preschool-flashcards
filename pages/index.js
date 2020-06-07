559c97f82f7fc598e248a4fe76fe5bf6f3aa681e
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 10:28:04 -0500

    use aliases

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Footer from 'components/Footer'
-import SiteHead from 'components/SiteHead'
-import Header from 'components/Header'
+import Footer from '@/components/Footer'
+import SiteHead from '@/components/SiteHead'
+import Header from '@/components/Header'
 
 const HomePage = () => (
   <div className="max-w-3xl p-4 m-auto text-center">
