95539f0ce2de53e961c5ed8f15a5e059c02e4ded
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 10:24:08 -0500

    add basic components

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer id="footer" className="mt-4">
+    <a href="#header">Back to top &uarr;</a>
+  </footer>
+)
+
+export default Footer
---
+++ components/SiteHead.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const SiteHead = () => (
+  <Head>
+    <title>Kindergarten Flashcards</title>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    <meta
+      name="description"
+      content="The alphabet, numbers, and fry (sight) words in flashcard form."
+    />
+  </Head>
+)
+
+export default SiteHead
