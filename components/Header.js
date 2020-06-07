0caa2de85dd809d5480411fc561cd96c69c7d5c9
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 11:03:30 -0500

    move nav to footer

--- components/Header.js
+++ components/Header.js
@@ -6,25 +6,6 @@ const Header = () => (
     <p className="text-1xl mb-4">
       The alphabet, numbers, and fry (sight) words in flashcard form.
     </p>
-    <nav className="mb-4">
-      <ul className="flex flex-wrap space-x-4 justify-center">
-        <li>
-          <Link href="/alphabet">
-            <a>Alphabet</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/numbers">
-            <a>Numbers</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/fry-words">
-            <a>Fry Words</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
   </header>
 )
 
