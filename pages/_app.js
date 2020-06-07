ae530e994206af1353ebb3c8883c043710942bf6
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 10:39:40 -0500

    add react-slick

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import '@/styles/index.css'
 
 const App = ({Component, pageProps}) => <Component {...pageProps} />
