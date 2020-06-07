0c0dff57589903a05ab5c8bc7b3518a2abe33a7e
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 15:32:02 -0500

    remove footer

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Link from 'next/link'
 import SiteHead from '@/components/SiteHead'
@@ -40,7 +39,6 @@ const HomePage = () => (
         keys to control the flashcards.
       </p>
     </main>
-    <Footer />
   </div>
 )
 
