d1af1e4c9d2af6b8615f6666f1a893dd12566e16
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 15:41:01 -0500

    adjust import order

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '@/styles/index.css'
+import PropTypes from 'prop-types'
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
-import PropTypes from 'prop-types'
+import '@/styles/index.css'
 
 const App = ({Component, pageProps}) => <Component {...pageProps} />
 
