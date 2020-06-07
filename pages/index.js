a66f46c23fd66d9da335399abc81416eee8189d3
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 18:45:01 -0500

    use menu from config

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import Link from 'next/link'
 import SiteHead from '@/components/SiteHead'
+import config from '@/lib/config'
 
 const HomePage = () => (
   <div className="max-w-3xl m-auto text-center">
@@ -8,31 +9,15 @@ const HomePage = () => (
     <Header />
     <main className="p-4">
       <div className="grid sm:grid-cols-3 gap-6">
-        <div className="border rounded-lg py-4 text-xl text-center hover:bg-gray-300">
-          <Link href="/alphabet">
-            <a>ABCs</a>
+        {config.menu.map((item, index) => (
+          <Link href={item.href} key={index}>
+            <a>
+              <div className="border rounded-lg py-4 text-xl text-center hover:bg-gray-300">
+                {item.label}
+              </div>
+            </a>
           </Link>
-        </div>
-        <div className="border rounded-lg py-4 text-xl text-center hover:bg-gray-300">
-          <Link href="/numbers">
-            <a>123s</a>
-          </Link>
-        </div>
-        <div className="border rounded-lg py-4 text-xl text-center hover:bg-gray-300">
-          <Link href="/shapes">
-            <a>Shapes</a>
-          </Link>
-        </div>
-        <div className="border rounded-lg py-4 text-xl text-center hover:bg-gray-300">
-          <Link href="/colors">
-            <a>Colors</a>
-          </Link>
-        </div>
-        <div className="border rounded-lg py-4 text-xl text-center hover:bg-gray-300">
-          <Link href="/fry-words">
-            <a>Fry Words</a>
-          </Link>
-        </div>
+        ))}
       </div>
       <p className="my-12">
         Click a box above to get started. You can swipe, click, or use the arrow
