636c8607adb04ffae77010a3557ec9869e222b80
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 14:21:07 -0500

    add margin

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => (
   <header id="header" className="max-w-sm m-auto">
     <h1 className="text-4xl mb-4">Kindergarten Flashcards</h1>
-    <p className="text-1xl mb-4">
+    <p className="text-1xl mb-12">
       The alphabet, numbers, and fry (sight) words in flashcard form.
     </p>
   </header>
