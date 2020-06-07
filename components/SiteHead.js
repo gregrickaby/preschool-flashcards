98c243dea80c6674ed3cfe59c99ba6d2ec8bf776
Greg Rickaby <greg@gregrickaby.com>
2020-06-07 11:07:29 -0500

    props for SiteHead

--- components/SiteHead.js
+++ components/SiteHead.js
@@ -1,18 +1,26 @@
+import PropTypes from 'prop-types'
 import Head from 'next/head'
 
-const SiteHead = () => (
+const SiteHead = (props) => (
   <Head>
-    <title>Kindergarten Flashcards</title>
+    <title>{props.title}</title>
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1, shrink-to-fit=no"
     />
     <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-    <meta
-      name="description"
-      content="The alphabet, numbers, and fry (sight) words in flashcard form."
-    />
+    <meta name="description" content={props.description} />
   </Head>
 )
+
+SiteHead.defeaultProps = {
+  title: 'Kindergarten Flashcards',
+  description: 'The alphabet, numbers, and fry (sight) words in flashcard form.'
+}
+
+SiteHead.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string
+}
 
 export default SiteHead
