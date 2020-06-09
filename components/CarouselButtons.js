ddf879de39ac419c30b8054286c665f25ef847b3
Greg Rickaby <greg@gregrickaby.com>
2020-06-08 21:32:04 -0500

    a11y

--- components/CarouselButtons.js
+++ components/CarouselButtons.js
@@ -2,9 +2,21 @@ import {ButtonPlay, ButtonBack, ButtonNext} from 'pure-react-carousel'
 
 const CarouselButtons = () => (
   <>
-    <ButtonBack className="text-4xl mr-12">⏪</ButtonBack>
-    <ButtonPlay className="text-4xl">⏯</ButtonPlay>
-    <ButtonNext className="text-4xl ml-12">⏩</ButtonNext>
+    <ButtonBack className="text-4xl mr-12">
+      <span role="img" aria-label="back">
+        ⏪
+      </span>
+    </ButtonBack>
+    <ButtonPlay className="text-4xl">
+      <span role="img" aria-label="play or pause">
+        ⏯
+      </span>
+    </ButtonPlay>
+    <ButtonNext className="text-4xl ml-12">
+      <span role="img" aria-label="forward">
+        ⏩
+      </span>
+    </ButtonNext>
   </>
 )
 
