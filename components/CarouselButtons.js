95a65e87ec25660c5e8519b2bcbd9167a5be904d
Greg Rickaby <greg@gregrickaby.com>
2020-06-08 21:29:50 -0500

    swap icons for emoji

--- components/CarouselButtons.js
+++ components/CarouselButtons.js
@@ -2,9 +2,9 @@ import {ButtonPlay, ButtonBack, ButtonNext} from 'pure-react-carousel'
 
 const CarouselButtons = () => (
   <>
-    <ButtonBack className="text-6xl mr-12">&lsaquo;</ButtonBack>
-    <ButtonPlay className="text-5xl">&#9654;</ButtonPlay>
-    <ButtonNext className="text-6xl ml-12">&rsaquo;</ButtonNext>
+    <ButtonBack className="text-4xl mr-12">⏪</ButtonBack>
+    <ButtonPlay className="text-4xl">⏯</ButtonPlay>
+    <ButtonNext className="text-4xl ml-12">⏩</ButtonNext>
   </>
 )
 
