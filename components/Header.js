6275b63cadd10c8e2b902d7a647025d3f56fb256
Greg Rickaby <greg@gregrickaby.com>
2020-07-28 09:51:02 -0500

    add site logo

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,13 @@ import config from '@/lib/config'
 
 const Header = () => (
   <header className="max-w-sm m-auto p-4">
-    <img className="mx-auto" src="/favicon/apple-icon.png" alt="site logo" />
+    <img
+      className="mx-auto"
+      src="/site-logo.png"
+      alt="site logo"
+      height="192"
+      width="192"
+    />
     <h1 className="mt-2 text-3xl font-bold leading-tight">{config.siteName}</h1>
     <p className="text-1xl my-4 italic">{config.siteDescription}</p>
   </header>
