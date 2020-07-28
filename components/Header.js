1c3c89d592ce09367f4a3240a22d5f612a42cb06
Greg Rickaby <greg@gregrickaby.com>
2020-07-28 09:47:24 -0500

    refactor -- no sticky

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,8 @@ import config from '@/lib/config'
 
 const Header = () => (
   <header className="max-w-sm m-auto p-4">
-    <h1 className="text-3xl font-bold leading-tight">{config.siteName}</h1>
+    <img className="mx-auto" src="/favicon/apple-icon.png" alt="site logo" />
+    <h1 className="mt-2 text-3xl font-bold leading-tight">{config.siteName}</h1>
     <p className="text-1xl my-4 italic">{config.siteDescription}</p>
   </header>
 )
