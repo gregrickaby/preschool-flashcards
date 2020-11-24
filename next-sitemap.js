24cbc48a55aa2fe6253355438ffa9dcaa9b4144a
Greg Rickaby <greg@gregrickaby.com>
2020-11-23 20:44:22 -0600

    add next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+// @see https://github.com/iamvishnusankar/next-sitemap
+module.exports = {
+  siteUrl: 'https://kindergarten-flashcards.now.sh/',
+  generateRobotsTxt: true,
+  changefreq: 'weekly'
+}
