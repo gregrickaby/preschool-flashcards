909850da01fba3ef9ff151e79bcfc17c354baf97
Greg Rickaby <gregrickaby@gmail.com>
2026-04-02 19:19:29 -0500

    update URLs to point to the new flashcards site

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 // @see https://github.com/iamvishnusankar/next-sitemap
 module.exports = {
-  siteUrl: 'https://kindergarten-flashcards.vercel.app/',
+  siteUrl: 'https://flashcards.gregrickaby.com/',
   generateRobotsTxt: true,
   changefreq: 'weekly'
 }
